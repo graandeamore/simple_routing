5287da6cc64d29d70c544e2a8f9c12f8746c0ca6
angel <grs2143@mail.ru>
2022-01-23 17:05:11 +0900

    react-course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
+import About from './About/About';
+import Cars from './Cars/Cars';
+import {Route, Routes, NavLink, Navigate} from 'react-router-dom'
+import CarDetail from "./CarDetail/CarDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        isLoggedIn: false
+    }
+
+    render() {
+
+        return (
+            <div>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/cars'>Cars</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <hr/>
+                <h3> Is logged in {this.state.isLoggedIn.toString()}</h3>
+                <button onClick={() => this.setState({isLoggedIn: !this.state.isLoggedIn})}>
+                    Login
+                </button>
+                <hr/>
+                {/* Routing */}
+                <Routes>
+                    <Route path="/" element={<h1>Home Page</h1>}/>
+                    {/* secure routing */}
+                    {this.state.isLoggedIn ? <Route path="/about" element={<About />}/> : null}
+                    <Route path="/cars" element={<Cars />}/>
+                    <Route path="/cars/:name" element={<CarDetail />}/>
+                    <Route path='*' element={<Navigate replace to='/'/>}/> {/* use at the very end */}
+                </Routes>
+                {/* Конец */}
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
---
+++ src/CarDetail/CarDetail.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CarDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    some car
+                </h1>
+
+            </div>
+        );
+    }
+}
+
+export default CarDetail;
